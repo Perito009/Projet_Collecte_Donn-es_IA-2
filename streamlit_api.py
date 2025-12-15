bc90136d31f7c7deb677d5a4e1c816a244091db3
O.Yann André <115635958+Perito009@users.noreply.github.com>
2025-12-15 15:08:02 +0000

    Fix: modification des affichage du graphiq en deux un pour le df.head et l'autre jut pour selectionné le type int et float du df ce qui evit l'erreur meltdata

--- streamlit_api.py
+++ streamlit_api.py
@@ -1,9 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-st.write("NOtre Application de Panne dans les 7 jours")
+st.title("Notre Application de Panne dans les 7 jours")
 
+df = pd.read_csv("energiTech_maintenance_sample.csv")
 
-df= pd.read_csv(energiTech_maintenance_sample.csv)
-st.line_chart(df)
+st.subheader("Aperçu des données")
+st.dataframe(df.head())
 
+df_numeric = df.select_dtypes(include=["int64", "float64"])
+
+st.subheader("Évolution des variables numériques")
+st.line_chart(df_numeric)
