1f86f401b1435ec8ddbd05994d6ebc0ea1d53df3
O.Yann André <115635958+Perito009@users.noreply.github.com>
2025-12-29 14:17:41 +0000

    Fix: corection du chemein api du .sh pour que le back se lance aussi dans render

--- main_project.sh
+++ main_project.sh
@@ -3,7 +3,7 @@ set -e
 echo "Starting application..."
 
 # Lancer l'API en arrière-plan
-python ApiPredictDays.py >> /workspaces/Projet_Collecte_Donn-es_IA-2/api_logs.log 2>&1 &
+python ApiPredictDays.py >> /Projet_Collecte_Donn-es_IA-2/api_logs.log 2>&1 &
 
 echo "API started in the background."
 # Lancer Streamlit (process principal)
